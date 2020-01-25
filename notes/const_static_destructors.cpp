c6838448d4c9078d4491554a5a8b34ea561dd584
Garrett <martin.garrettl@gmail.com>
2020-01-25 15:48:25 -0800

    in class notes

---
+++ notes/const_static_destructors.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class ass {
+  private:
+    int x;
+    int y;
+  public:
+    int get_x();
+    int get_y();
+    void set_x(int);
+    void set_y(int);
+};
+
+int main() {
+
+
+  return 0;
+}
