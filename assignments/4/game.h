79b084c467542250ea6776cd764db34670a73878
Garrett <martin.garrettl@gmail.com>
2020-02-18 16:54:18 -0800

    add assignment 4

---
+++ assignments/4/game.h
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class game {
+  private:
+  public:
+};
+
+#endif
---
+++ assignments/4/room.h
@@ -0,0 +1,16 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class room {
+  private:
+    int room_x, room_y;
+  public:
+    
+};
+
+#endif
